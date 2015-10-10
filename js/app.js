c344f8bdd350c4eee03ca5a73cba530c60d9d51b
MichaelLSmith <mlsmith45@gmail.com>
2015-10-10 13:17:32 -0400

    add timestamp; begin add searchTitle; reorganise code based on John's structure

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $('#createEntry').submit(function(event){
         event.preventDefault();
         var frm = $('#createEntry');
-        createEntry(frm);
+        entryForm(frm);
     });
 
     $('#search').submit(function(event){
@@ -25,63 +25,116 @@ $(document).ready(function(){
     var currentEntryContent;
     var currentEntryAuthor;
     var modalHtml;
-
-// Push HTML Functions
-function generateHtml(currentEntryTitle){
-    var html = '<li>'+'<a href="#'+currentEntryTitle+'"'+'>'+currentEntryTitle+'</a>'+'</li>';
-    return html;
-}
-function generateEntryModal(currentEntryTitle,currentEntryContent,currentEntryAuthor){
-    var modalHtml = '<div id="'+currentEntryTitle+'"'+'class="modalDialog"' + '>'+'<div>'+'<a href="#close" title="Close" class="close">X</a>'+'<h2>'+currentEntryTitle+'</h2>'+'<p>'+currentEntryContent+'</p>'+'</div>'+'</div>'
-    // console.log(modalHtml);
-    return modalHtml;
-}
+    var counter;
+    var timestamp;
 
 //Helper Functions
 
-var basicLoop = function(againstMeasure){
-    for(var counter=0; counter < againstMeasure; counter++)
+function basicLoop(againstMeasure, callback) {
+    for(var i = 0; i < againstMeasure.length; i++) {
+        callback(i, againstMeasure[i]);
+        // console.log(callback);
+        console.log(againstMeasure);
+    }
 }
 
-// Main Create Entry Function tied to Create Function Form.
-function createEntry(frm){
-    //collect values from form
-    title = frm.find('input[name="title"]').val();
-    content = frm.find('input[name="content"]').val();
-    tags = frm.find('input[name="tags"]').val();
-    author = frm.find('input[name="author"]').val();
-    //create new entry
-    MyJournal.publish(title,content,author);
+//Model 1
 
-    //generate html
-    // var html = generateHtml();
-    // $('#response').html(html);
-}
-
-// Journal Contructor and prototype Functions
+//1.1 Journal Constructor
 
 function Journal(){
     this.entries = [];
-} 
+}
 
-Journal.prototype.publish = function(title,content,author){
-    this.entries.push(new entry(title,content,author));
-    // timestamp
-        var d = new Date();
-        console.log(d);
-        this.entries.timestamp = d;
-        this.entries[0].timestamp = d;
-    // generate html
-        // currentEntryTitle = title;
-        // console.log(currentEntryTitle);
+//1.2 Entry Constructor
 
+function entry(title, content, author, timestamp){
+    this.title = title;
+    this.content = content;
+    this.author = author;
+    this.timestamp = timestamp;
+    this.tags = [];
+}
+
+//Model 2 Prototype Functions to add content
+
+// 2.1 Journal basic create entry
+
+Journal.prototype.createEntry = function(title,content,author,timestamp){
+    //timestamp
+    this.entries.push(new entry(title,content,author,timestamp));
+    console.log(title+content+author+timestamp);
+    //call to publish entry function - written below.
+};
+
+// 2.2 Add Tags to Entries
+
+entry.prototype.addTag = function(tag){
+        this.tags.push(tag);
+    }
+
+
+// Model 3 (Is this the Controler? Ask John)
+// 3.1 Search all values of entries
+
+Journal.prototype.searchEntries = function(searchString) {
+        var result = false;
+        for (var entriesIndex = 0; entriesIndex < this.entries.length; entriesIndex++) { 
+            if (
+                this.entries[entriesIndex].title.search(searchString) > -1 || 
+                this.entries[entriesIndex].content.search(searchString) > -1 ||
+                this.entries[entriesIndex].author.search(searchString) > -1 ||
+                this.entries[entriesIndex].tags.search(searchString) > -1
+                ){
+                    console.log(this.entries[entriesIndex]);
+                    result = true;
+                }
+        return = result;
+        }//end of For
+        if (result = false){
+            console.log('Could not find any entry containing'+' '+searchString);
+        }
+    }//end searchEntries function
+
+//3.2 Search by Title
+
+Journal.prototype.searchTitle = function(searchString){
+    // var result = false;
+    var searchTitleResults = [];
+    
+    for(var entriesIndex = 0; entriesIndex < this.entries.length; entriesIndex++){
+        if (this.entries[entriesIndex].title.search(searchString) > -1)
+
+            //Generate List of Search Results by Title
+             // generate html
         var html = generateHtml(title);
         // console.log(html);
-        $("#response").append(html);
-        var modalHtml = generateEntryModal(title,content,author);
-        // console.log(modalHtml);
-        $('#dummyModal').before(modalHtml);
-    };
+        $("#searchResults").html(html);
+
+
+            this.entries[entriesIndex].push(searchTitleResults);
+         // searchResults.push(// entriesIndex);
+        console.log(this.entries[entriesIndex]);
+    }
+
+    return searchTitleResults;
+    //searchTitleResults array will contain the index numbers of the matching entries with entries array.
+}
+
+Journal.prototype.getEntries = function(entryList) {
+
+    var entryResults = [];
+    // for each entryID in entryList loop to access entries in entryList
+        entryResults.push(this.entries[entryID])
+    return entryResults;
+}
+
+    // myJournal.getEntries(myJournal.searchTitle("text"));
+
+//  entry Functions:
+
+//2.2.1 Search Tags
+
 Journal.prototype.searchTags = function(searchString) {
         for (var entriesIndex = 0; entriesIndex < this.entries.length; entriesIndex++) { 
             for (var tagsIndex = 0; tagsIndex < this.entries[entriesIndex].tags.length; tagsIndex++){      
@@ -93,63 +146,102 @@ Journal.prototype.searchTags = function(searchString) {
         }
     } // end SearchTags function
 
-Journal.prototype.searchEntries = function(searchString) {
-        var result = false;
-        for (var entriesIndex = 0; entriesIndex < this.entries.length; entriesIndex++) { 
-            if (
-                this.entries[entriesIndex].title.search(searchString) > -1 || 
-                this.entries[entriesIndex].content.search(searchString) > -1 ||
-                this.entries[entriesIndex].author.search(searchString) > -1
-                ){
-                    console.log(this.entries[entriesIndex]);
-                    result = true;
-                }
-        }//end of For
-    }//end searchEntries function
-
-Journal.prototype.searchTitle = function(searchString){
-    var result = false;
-    for(var entriesIndex = 0; entriesIndex< this.entries.length; entriesIndex++){
-        if(this.entries[entriesIndex].title.search(searchString) > -1)
-            console.log(this.entries[entriesIndex]);
-            result = true;
-    }
-}
 
 
 
-//Display Entries
 
-// Journal.prototype.displayEntries = function(){
-//     for (var tagsIndex=0; tagsIndex < this.entries.length; entriesIndex++){
-//         //convert each entry into html
 
+
+//     for(var entriesIndex = 0; entriesIndex< this.entries.length; entriesIndex++){
+//         if(this.entries[entriesIndex].title.search(searchString) > -1)
+//             console.log(this.entries[entriesIndex]);
+//             result = true;
 //     }
 // }
 
 
-// entry Functions:
-
-function entry(title, content, author){
-    this.title = title;
-    this.content = content;
-    this.author = author;
-    var timestamp = timestamp;
-    this.tags = [];
+// Push HTML Functions
+function generateAllEntriesHtml(currentEntryTitle){
+    var html = '<li>'+'<a href="#'+currentEntryTitle+'"'+'>'+currentEntryTitle+'</a>'+'</li>';
+    return html;
 }
-entry.prototype.addTag = function(tag){
-        this.tags.push(tag);
+
+//Search Result Funtions
+
+// 1. Generate List Heading and <ul>
+    function generateSearchListHeadingHtml(currentEntryTitle){
+        var html = '<p>'+'Search Results:'+'</p>'+'<ul>';
+        return html;
     }
+
+// 2. Generate Search Results in <li>
+    function generateSearchListItemsHtml(currentEntryTitle){
+        var html = '<li>'+'<a href="#'+currentEntryTitle+'"'+'>'+currentEntryTitle+'</a>'+'</li>'+'</ul>';
+    }
+
+//Generate Items Modals
+    function generateEntryModal(currentEntryTitle,currentEntryContent,currentEntryAuthor){
+        var modalHtml = '<div id="'+currentEntryTitle+'"'+'class="modalDialog"' + '>'+'<div>'+'<a href="#close" title="Close" class="close">X</a>'+'<h2>'+currentEntryTitle+'</h2>'+'<p>'+currentEntryContent+'</p>'+'</div>'+'</div>'
+        // console.log(modalHtml);
+        return modalHtml;
+}
+
+//Helper Functions
+
+// function basicLoop (againstMeasure){
+
+//Johnson's 3 buttons in one form code
+function bindListners(){
+ $('.edit').unbind('submit');
+ $('.edit').submit(function(event){
+     event.preventDefault();
+     var frm = $(this);
+     var btn = frm.find("button:focus").attr('data-btn');
+     if (btn == "add-tag"){
+       addTagFromForm(frm);
+     } else if (btn == "delete"){
+       deleteFromEntry(frm);
+     } else {
+       populateModal(frm);
+     }
+ });
+}
+
+// Main Create Entry Function tied to Create Function Form.
+function entryForm(frm){
+    //collect values from form
+    title = frm.find('input[name="title"]').val();
+    content = frm.find('textarea[name="content"]').val();
+    tags = frm.find('input[name="tags"]').val();
+    author = frm.find('input[name="author"]').val();
+    //timestamp
+    timestamp = new Date();
+    console.log(title+content+author+timestamp);
+    //create new entry
+    MyJournal.createEntry(title,content,author,timestamp);
+}
+
+
+   // generate html
+        var html = generateAllEntriesHtml(title);
+        // console.log(html);
+        $("#response").append(html);
+        var modalHtml = generateEntryModal(title,content,author);
+        // console.log(modalHtml);
+        $('#dummyModal').before(modalHtml);
+    // this.entries[1].timestamp = d;
+    };
 
 
 var MyJournal = new Journal();
-MyJournal.publish('jogging', 'I went Jogging', 'Michael');
-MyJournal.publish('sailing', 'I went sailing', 'Michael');
-MyJournal.publish('reading', 'I spent the afternoon reading.', 'Michael');
+MyJournal.createEntry('jogging', 'I went Jogging', 'Michael');
+MyJournal.createEntry('sailing', 'I went sailing', 'Michael');
+MyJournal.createEntry('sailing on a tuesday', 'I went sailing on tuesday', 'Michael');
+MyJournal.createEntry('reading', 'I spent the afternoon reading.', 'Michael');
 MyJournal.entries[1].addTag('sailing');
 MyJournal.entries[0].addTag('jogging');
 MyJournal.entries[1].addTag('sports');
 MyJournal.entries[0].addTag('sports');
 MyJournal.entries[0].addTag('Friday');
-MyJournal.searchEntries('went');
-MyJournal.searchTags('Friday');
+// MyJournal.searchEntries('went');
+// MyJournal.searchTags('Friday');
